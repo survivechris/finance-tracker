66debaa155535d280a941d6fec6fb4edce280742
survivechris <survival_of_chris@hotmail.com>
2017-06-08 22:24:54 +0800

    use Scaffold to create join model UserStock, add association and adjust its controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504133458) do
+ActiveRecord::Schema.define(version: 20170608133159) do
 
   create_table "stocks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "ticker"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20170504133458) do
     t.decimal  "last_price", precision: 10
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+  end
+
+  create_table "user_stocks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.integer  "stock_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
