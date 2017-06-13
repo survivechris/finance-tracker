960ca32e66a6fbba33bf48b5e5a14b6b9e99a6b6
survivechris <survival_of_chris@hotmail.com>
2017-06-13 21:52:32 +0800

    change the root path to welcome, so that we could logout the user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#my_portfolio'
+  root 'welcome#index'
 
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
